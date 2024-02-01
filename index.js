eb75b00c21b9d1f1b1d4e2562ec1d304d2d9c689
PreziosiManuel <manuelpreziosirace@gmail.com>
2024-02-01 17:00:30 +0100

    commit

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const creaCelle = () => {
   for (let i = 1; i < 100; i++) {
     const divCella = document.createElement("div");
     divCella.className = "cella";
-    // board.innerHTML += `<div class='cell'>${i + 1}</div>
 
     const elementoCella = document.createElement("h3");
     elementoCella.innerText = i;
@@ -12,17 +11,14 @@ const creaCelle = () => {
     tab.appendChild(divCella);
   }
 };
+
 creaCelle();
 
 const bottone = document.getElementById("btn");
 bottone.onclick = () => {
-  const numberSelect = document.querySelectorAll("h3");
-  const numeroExtract = Math.floor(math.random() * cella.length);
-  numeroRandom = cella[numeroExtract];
-  numeroExtract.className = "green";
+  const celle = document.querySelectorAll(".cella");
+  const randomNum = Math.floor(Math.random() * celle.length);
+  const randomCell = celle[randomNum];
+  randomCell.style.backgroundColor = "green";
+  console.log(randomCell.innerText);
 };
-
-// const getRandomNum = function (range) {
-//     const randIndex = Math.floor(Math.random() * range.length)
-//     const random = range.splice(randIndex, 1)[0]
-//     return random
